52da8fd5a0db8107c1063e237d5d6dae026e7f43
Phong <p7157568@gmail.com>
2016-05-21 18:00:26 +0700

    hiện thực Interactor interface bằng cách extend class AbstractInteractor và hiện thực run()

--- app/src/main/java/com/kodelabs/boilerplate/domain/interactors/WelcomingInteractorImpl.java
+++ app/src/main/java/com/kodelabs/boilerplate/domain/interactors/WelcomingInteractorImpl.java
@@ -1,9 +1,29 @@
 package com.kodelabs.boilerplate.domain.interactors;
 
+import com.kodelabs.boilerplate.domain.interactors.base.AbstractInteractor;
+
 /**
  * Created by huynhducthanhphong on 5/21/16.
  */
 
-// TODO: 5/21/16 5
-public class WelcomingInteractorImpl {
+// TODO: 5/21/16 5 hiện thực Interactor interface bằng cách extend class AbstractInteractor và hiện thực run()
+public class WelcomingInteractorImpl extends AbstractInteractor implements WelcomingInteractor{
+
+    @Override
+    public void run() {
+        // retrieve the message
+        final String message = mMessageRepository.getWelcomeMessage();
+
+        // check if we have failed to retrieve our message
+        if (message == null || message.length() == 0) {
+
+            // notify the failure on the main thread
+            notifyError();
+
+            return;
+        }
+
+        // we have retrieved our message, notify the UI on the main thread
+        postMessage(message);
+    }
 }
